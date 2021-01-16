fc21f8226a4865363b17b7be7a3f96f4baa1c5e8
masamitsu-miyoshi <masa_yamato@outlook.jp>
2021-01-16 19:47:59 +0900

    databasesession

--- config/app.php
+++ config/app.php
@@ -398,6 +398,6 @@ return [
      * To use database sessions, load the SQL file located at config/schema/sessions.sql
      */
     'Session' => [
-        'defaults' => 'php',
+        'defaults' => 'database',
     ],
 ];
